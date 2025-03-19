96fce141bd1495a81caf5165f3bc16a20d4c6eeb
rodrigo <rdrgtrpp@gmail.com>
2025-03-19 11:20:23 +0200

    mesage: improving style for MealView

--- src/views/MealView.vue
+++ src/views/MealView.vue
@@ -1,8 +1,17 @@
 <template>
-  <div v-if="meal" class="rounded-lg shadow-lg bg-[#FFF9F0]  m-3 p-3">
-    <h2>{{ meal.strMeal }}</h2>
-    <img :src="meal.strMealThumb" :alt="meal.strMeal">
-    <pre>{{ {...meal} }}</pre>
+  <div v-if="meal" class="rounded-lg shadow-lg bg-[#FFF9F0] m-3 p-3">
+    <h2 class="text-[#E67E22] text-lg my-6">{{ meal.strMeal }}</h2>
+    <div class="flex flex-row justify-start gap-3">
+      <img :src="meal.strMealThumb" :alt="meal.strMeal" class="w-[30%] h-max" />
+      <ul>
+        <li>Area: {{ meal.strArea }}</li>
+        <li>Category: {{ meal.strCategory }}</li>
+        <li class="text-justify">Instructions: {{ meal.strInstructions }}</li>
+        <!-- <li v-for=""></li> -->
+        <!-- <pre>{{ { ...meal } }}</pre> -->
+      </ul>
+
+    </div>
   </div>
   <div v-else>Loading meal...</div>
 </template>
